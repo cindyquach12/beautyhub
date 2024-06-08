a4b8314c620578343c045d66f89b42b43acf766c
Chris <cvo13014@gmail.com>
2024-06-07 17:33:46 -0700

    changed business card styling to always be white and red

--- app/components/BusinessCard.tsx
+++ app/components/BusinessCard.tsx
@@ -19,15 +19,15 @@ export const BusinessCard: FunctionComponent<BusinessCardProps> = ({
     imgSrc,
 }) => {
     return (
-        <div className="bg-white/95 border rounded-sm border-border/70 h-[18.75rem] overflow-hidden shadow-md">
-            <div className="business-details text-black border-b border-border/40 pl-3 py-2">
+        <div className="bg-red-400 border rounded-sm border-border/70 h-[18.75rem] overflow-hidden shadow-md">
+            <div className="business-details text-white border-b border-border/40 pl-3 py-2">
                 <h4 className="text-lg font-medium ">{name}</h4>
                 <div className="flex items-center my-1">
                     <p>{`${rating}`}</p>
                     <Star className="ml-1 mr-2 size-[1rem]" />
                     <p className="text-sm">{`${reviewCount}`} reviews</p>
                 </div>
-                <p className="text-xs text-gray-400">{location}</p>
+                <p className="text-xs text-white">{location}</p>
             </div>
             <img
                 src={imgSrc}
